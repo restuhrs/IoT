31850f7ab4ab918fd09b97c0889d3331242f7cd2
Restu <restukharisma858@gmail.com>
2025-04-15 20:01:31 +0700

    delete file esp32 kelembaban suhu

---
+++ latihanESP32SensorSuhuKelembaban/.pio/build/esp32doit-devkit-v1/FrameworkArduino/FirmwareMSC.cpp.d
@@ -0,0 +1,5 @@
+.pio/build/esp32doit-devkit-v1/FrameworkArduino/FirmwareMSC.cpp.o: \
+ /Users/mac/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
+ /Users/mac/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
+ /Users/mac/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ /Users/mac/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
