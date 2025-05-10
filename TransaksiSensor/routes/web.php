628c27969151fe0bbaa73d0137854231e4184a91
Restu <restukharisma858@gmail.com>
2025-05-10 23:00:11 +0700

    praktikum bab 15

--- TransaksiSensor/routes/web.php
+++ TransaksiSensor/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\GraphController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [GraphController::class, 'index'])->name('graph');
+Route::get('/graph/export', [GraphController::class, 'exportToExcel'])->name('graph.export');
